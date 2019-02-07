aaae57413d79d7195341b5add57616affc1cfad2
daisukeshun <40919307+daisukeshun@users.noreply.github.com>
2019-02-07 14:13:39 +0300

    Read the mesh

--- main/cube.py
+++ main/cube.py
@@ -5,7 +5,13 @@ class Triangle:
     def __init__(self, p1, p2, p3):
         self.points = [p1, p2, p3]
 
-def meshLoad(string):
+class mesh:
+    def __init__(self, string):
+        self.points = vertexLoad(string)
+        self.faces = faceLoad(string)
+
+
+def vertexLoad(string):
     vertices = []
     f = open(string)
     for x in f:
@@ -18,21 +24,39 @@ def meshLoad(string):
     return vertices
 
 
+def faceLoad(string):
+    f = open(string)
+    faces = []
+    for x in f:
+        if (x[0] == 'f'):
+            a = x.split()
+            a.remove('f')
+            for i in range(len(a)):
+                a[i] = int((a[i][:a[i].index('/')]))
+            faces.append(a)
+    return faces
+
 W = 600
 H = 600
 
 def main():
     win = GraphWin("Graph window", W, H)
 
-    a1 = Point3D(10, 10, 10)
-    a2 = Point3D(10, 20, 1)
-    a3 = Point3D(20, 20, 1)
+    a1 = Point3D(0, 0, 1)
+    a2 = Point3D(0, 1, 1)
+    a3 = Point3D(1, 1, 1)
 
-    b = Triangle3D(a1, a2, a3, 10, W, H)
+    b = Triangle3D(a1, a2, a3, 100, W, H)
     b.draw3D(win)
 
     win.getMouse()
     win.close()
     print(win.winfo_width)
 
-main()
+# main()
+
+
+m = mesh('45-trees/tree01.obj')
+print(m.points)
+print('\n\n\n')
+print(m.faces)
--- main/mymath.py
+++ main/mymath.py
@@ -9,11 +9,11 @@ class Point3D:
 
 class Triangle3D:
     def __init__(self, p1, p2, p3, focal_length, width, height):
-        self.points = []
+        self.points = [0,0,0]
         self.points[0] = TranslateTo2D(p1, focal_length, width, height)
         self.points[1] = TranslateTo2D(p2, focal_length, width, height)
         self.points[2] = TranslateTo2D(p3, focal_length, width, height)
-        
+
     def draw3D(self, graphic):
         p = self.points
         Line(p[0], p[1]).draw(graphic)

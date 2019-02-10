93b65d4bed959664500186ce19dcdeeb2979018f
daisukeshun <40919307+daisukeshun@users.noreply.github.com>
2019-02-10 07:10:18 +0300

    Add functions for rotating objects in space

--- main/mesh.py
+++ main/mesh.py
@@ -4,6 +4,8 @@ from mymath import *
 class Triangle3D:
     def __init__(self, p1, p2, p3, width, height, focal_length):
         self.points = [0, 0, 0]
+
+
         self.points[0] = TranslateTo2D(p1, width, height, focal_length)
         self.points[1] = TranslateTo2D(p2, width, height, focal_length)
         self.points[2] = TranslateTo2D(p3, width, height, focal_length)
@@ -25,25 +27,18 @@ class Square3D:
         print(p1, p2, p3, p4)
         self.points = [0, 0, 0, 0]
 
-        # p1[2] += 3
-        # p2[2] += 3
-        # p3[2] += 3
-        # p4[2] += 3
-
-
-
         self.points[0] = TranslateTo2D(p1, width, height, focal_length)
         self.points[1] = TranslateTo2D(p2, width, height, focal_length)
         self.points[2] = TranslateTo2D(p3, width, height, focal_length)
         self.points[3] = TranslateTo2D(p4, width, height, focal_length)
         print(self.points)
+
     def draw3D(self, graphic):
         p = self.points
         Line(p[0], p[1]).draw(graphic)
         Line(p[1], p[2]).draw(graphic)
         Line(p[2], p[3]).draw(graphic)
         Line(p[3], p[0]).draw(graphic)
-
 
     def drawPoints(self, graphic):
         p = self.points
@@ -63,6 +58,12 @@ class mesh:
                 p1 = m.points[i[0]-1]
                 p2 = m.points[i[1]-1]
                 p3 = m.points[i[2]-1]
+
+                p1 = rotateX(40, p1)
+                p2 = rotateX(40, p2)
+                p3 = rotateX(40, p3)
+
+
                 tr = Triangle3D(p1, p2, p3, width, height, focal_length)
                 # tr.drawPoints(window)
                 tr.draw3D(window)
@@ -72,6 +73,12 @@ class mesh:
                 p2 = m.points[i[1]-1]
                 p3 = m.points[i[2]-1]
                 p4 = m.points[i[3]-1]
+
+                p1 = rotateX(40, p1)
+                p2 = rotateX(40, p2)
+                p3 = rotateX(40, p3)
+                p4 = rotateX(40, p4) 
+
                 sq = Square3D(p1, p2, p3, p4, width, height, focal_length)
                 # sq.drawPoints(window)
                 sq.draw3D(window)
@@ -81,6 +88,10 @@ class mesh:
         for item in win.items[:]:
             item.undraw()
         win.update()
+        
+
+
+
 
 def vertexLoad(string):
     vertices = []
--- main/mymath.py
+++ main/mymath.py
@@ -10,9 +10,9 @@ def matmulvec(i_vec, width, height, focal_length):
     print(i_vec[0], i_vec[1], i_vec[2])
     zFar = 1000
     zNear = 0.1
-    w = i_vec[2] + 3  
-    _x =(i_vec[0])*width/height * width/2
-    _y = (i_vec[1])*width/height * height/2
+    w = i_vec[2] + 3
+    _x =(i_vec[0])* width/height * width/2
+    _y = -(i_vec[1]) * height/2
     _z = i_vec[2] * (zFar/(zFar - zNear)) - zFar*zNear/(zFar - zNear)
     if w != 0:
         _x /= w
@@ -28,6 +28,33 @@ def matmulvec(i_vec, width, height, focal_length):
     return a
 
 
+def mmult(i, m):
+    o = []
+    o.append(i[0] * m[0][0] + i[1]*m[1][0] + i[2]*m[2][0])
+    o.append(i[0] * m[0][1] + i[1]*m[1][1] + i[2]*m[2][1])
+    o.append(i[0] * m[0][2] + i[1]*m[1][2] + i[2]*m[2][2])
+    return o
+
 def TranslateTo2D(Point3, width, height, focal_length):
         res = matmulvec(Point3, width, height, focal_length)
         return res
+
+def rotateX(angle, point):
+    angle = radians(angle)
+    m = [[1, 0, 0],
+        [0, cos(angle), -sin(angle)],
+        [0, sin(angle), cos(angle)]]    
+    res = mmult(point, m)
+    print(res)
+    return res
+
+
+def rotateY(angle, point):
+    angle = radians(angle)
+    m = [[cos(angle), 0, sin(angle)],
+         [0, 1, 0],
+         [-sin(angle), 0, cos(angle)]]
+
+    res = mmult(point, m)
+    print(res)
+    return res

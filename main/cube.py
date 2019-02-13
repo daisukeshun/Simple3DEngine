d2cd0c6c883b461963cef1da0cefbe0bb3415309
daisukeshun <40919307+daisukeshun@users.noreply.github.com>
2019-02-13 08:21:54 +0300

    FPS Camera

--- main/cube.py
+++ main/cube.py
@@ -44,13 +44,13 @@ def main():
         if keys_pressed[pygame.K_w]:
                 Camera[2] -= 0.2
         if keys_pressed[pygame.K_a]:
-                # Camera[0] -= 0.2
-                ss -= 1
+                Camera[0] += 0.2
+                ss -= 3
         if keys_pressed[pygame.K_s]:
                 Camera[2] += 0.2
         if keys_pressed[pygame.K_d]:
-                # Camera[0] += 0.2
-                ss += 1
+                Camera[0] -= 0.2
+                ss += 3
         pygame.draw.rect(win, BLACK, (0,0, W, H))
         b.meshDraw(win, W, H, focal_length, ss, Camera)
         a.meshDraw(win, W, H, focal_length, ss, Camera)
